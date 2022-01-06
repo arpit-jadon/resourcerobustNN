0b14f9d728bb98b5842556616fa0aa73e3cd9356
mlcysec_team002 <mlcysec_team002@conduit2.cs.uni-saarland.de>
2022-01-06 01:32:17 +0100

    setting gpu to 1 for now

--- RoCL_branch/sleep.sh
+++ RoCL_branch/sleep.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # file name: sleep.sh
 
-python -m torch.distributed.launch --nproc_per_node=2 rocl_train.py --ngpu 2 --batch-size=256 --model='ResNet18' --k=7 --loss_type='sim' --advtrain_type='Rep' --attack_type='linf' --regularize_to='other' --attack_to='other' --train_type='contrastive' --dataset='cifar-10'
+python -m torch.distributed.launch --nproc_per_node=1 rocl_train.py --ngpu 1 --batch-size=256 --model='ResNet18' --k=7 --loss_type='sim' --advtrain_type='Rep' --attack_type='linf' --regularize_to='other' --attack_to='other' --train_type='contrastive' --dataset='cifar-10'
