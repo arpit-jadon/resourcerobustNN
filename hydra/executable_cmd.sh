9aa54fc0f2d49a56f02a1209d3b799b15206669d
nik1806 <nikhil.paliwal14@gmail.com>
2022-01-10 22:24:01 +0100

    Add: hydra finetuning

--- hydra/executable_cmd.sh
+++ hydra/executable_cmd.sh
@@ -3,13 +3,12 @@ python train.py --arch resnet18 --exp-mode pretrain --configs configs/configs.ym
     --trainer adv --val_method adv --k 1.0  --epochs 2 --save-dense
 SKIP
 
-
+<< SKIP
 python train.py --arch resnet18 --exp-mode prune --configs configs/configs.yml\
     --trainer adv --val_method adv --k 0.1 --scaled-score-init\
     --source-net ./trained_models/temp/pretrain/latest_exp/checkpoint/checkpoint.pth.tar --epochs 1 --save-dense
-
-<< SKIP
-python train.py --arch resnet18 --exp-mode finetune --configs configs/configs.yml\
-    --trainer adv --val_method adv --k 0.1 --source-net pruned_net_checkpoint_path\
-    --save-dense --lr 0.01 --epochs 1
 SKIP
+
+python train.py --arch resnet18 --exp-mode finetune --configs configs/configs.yml\
+    --trainer adv --val_method adv --k 0.1 --source-net ./trained_models/temp/prune/latest_exp/checkpoint/checkpoint.pth.tar\
+    --save-dense --lr 0.01 --epochs 1
