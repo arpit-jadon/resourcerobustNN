ceb0a50d434aed9dfff3ff4162a6776e578e992b
nik1806 <nikhil.paliwal14@gmail.com>
2022-01-28 08:27:26 +0100

    Rocl exp2 results

--- hydra/executable_cmd.sh
+++ hydra/executable_cmd.sh
@@ -5,17 +5,26 @@ python train.py --arch resnet18 --exp-mode pretrain --configs configs/configs.ym
 python train.py --arch resnet18 --exp-mode prune --configs configs/configs.yml\
     --trainer base --val_method adv --k 0.1 --scaled-score-init --exp-name res18_base_adv\
     --source-net ./trained_models/res18_base_adv/pretrain/latest_exp/checkpoint/checkpoint.pth.tar --epochs 20 --save-dense
-SKIP
 
-<< SKIP
 python train.py --arch resnet18 --exp-mode finetune --configs configs/configs.yml\
     --trainer base --val_method adv --k 0.1 --source-net ./trained_models/res18_base_adv/prune/latest_exp/checkpoint/checkpoint.pth.tar\
     --save-dense --lr 0.01 --epochs 20 --exp-name res18_base_adv
 SKIP
 
+<< SKIP
+SKIP
+
+python train.py --arch resnet18 --exp-mode pretrain --configs configs/configs.yml\
+    --trainer adv --val_method adv --k 1.0  --epochs 25 --save-dense --exp-name rocl_ext_adv\
+    --source-net ./trained_models/rocl_ckpt_300epoch --load_RoCL extractor
+
 python train.py --arch resnet18 --exp-mode prune --configs configs/configs.yml\
-    --trainer adv --val_method adv --k 0.1 --scaled-score-init --exp-name rocl_adv\
-    --source-net ./trained_models/ckpt1 --epochs 1 --save-dense
+    --trainer adv --val_method adv --k 0.1 --scaled-score-init --exp-name rocl_ext_adv\
+    --source-net ./trained_models/rocl_ext_adv/pretrain/latest_exp/checkpoint/checkpoint.pth.tar --epochs 25 --save-dense
+
+python train.py --arch resnet18 --exp-mode finetune --configs configs/configs.yml\
+    --trainer adv --val_method adv --k 0.1 --source-net ./trained_models/rocl_ext_adv/prune/latest_exp/checkpoint/checkpoint.pth.tar\
+    --save-dense --lr 0.01 --epochs 25 --exp-name rocl_ext_adv
 
 << skip
 python train.py --arch resnet18 --exp-mode pretrain --configs configs/configs.yml\
